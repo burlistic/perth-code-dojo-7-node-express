a0919e98a67874b70c2972cf05e2c720412162d0
burlistic <burlistic@yahoo.co.uk>
2015-02-13 11:52:11 +0800

    Added CORS support for Express API

--- PerthCodeDojo/app.js
+++ PerthCodeDojo/app.js
@@ -10,6 +10,13 @@ var users = require('./routes/api/users');
 
 var app = express();
 
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
