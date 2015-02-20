f208ee2cee75a547668c40f6e7fa3fcba8ef3868
burlistic <burlistic@yahoo.co.uk>
2015-02-20 14:20:20 +0800

    Added Peter

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
-  res.json({users : [{firstName : "William", lastName : "Webster"}]});
+  res.json({users : [{firstName : "William", lastName : "Webster"}, {firstName : "Peter", lastName : "Gasparik"}]});
 });
 
 module.exports = router;
