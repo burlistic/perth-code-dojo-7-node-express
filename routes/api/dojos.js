7ef2aeedd0b51dd5ad2a07b43b02b138ed3866e2
Stephen Price <Stephen@lythixdesigns.com>
2015-02-23 19:43:33 +0800

    Details backend

--- routes/api/dojos.js
+++ routes/api/dojos.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+// router.param('id', function (req, res, next, id) {
+//  console.log('CALLED ONLY ONCE');
+//  next();
+// });
+// router.param('id', /^\d+$/);
+
+router.get('/:id', function (req, res, next) {
+  console.log('although this matches too');
+  
+  res.json({description : "github", link : "http://google.com", slides: "http://github.com"});
+ 
+  next();
+});
+
+
 /* GET Dojo listing. */
 router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
@@ -11,5 +26,8 @@ router.get('/', function(req, res, next) {
    {title : "Aardvaark", date : "2015-04-01"}
    ]});
 });
+
+
+
 
 module.exports = router;
